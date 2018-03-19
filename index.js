f6083c302c0d4295afbeab11ac2a9dbda0b46065
Ray Snider <ray.snider21@gmail.com>
2018-03-19 20:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function happyHolidays()
 {
   return "Happy holidays!";
 }
+
+function happyHolidaysTo(name)
+{
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidaysTo(holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(days, holiday)
+{
+  return `It's ${days} days until ${holiday}!`;
+}
